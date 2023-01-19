2f8e7789d9fdadd6458e58b5769860118f6e710e
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-01-19 19:53:09 +0530

    Info Upfate

--- src/coponant/Header/header.js
+++ src/coponant/Header/header.js
@@ -4,18 +4,59 @@ const Header = () => {
   return (
     <>
       <div className="header_main">
-         <h1> <i className="fa-solid fa-rectangle-list"></i> Make Your Day Here</h1>
+        <h1> <i className="fa-solid fa-rectangle-list"></i> Make Your Day Here</h1>
 
         <div className="info">
           <p className='m-0'>
             <a className="btn btn-primary px-2 py-0 bg-dark" data-bs-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
               <i className="fa-solid fa-circle-info"></i> Show Info
             </a>
-          
+
           </p>
-          <div className=" collapse" id="collapseExample1">
-            <div className=" info_content card card-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum harum ea modi tempora odit nihil, ratione minus minima magnam delectus deleniti alias possimus, dolorem unde numquam! Maxime, error culpa doloribus, possimus quas aspernatur at unde ducimus laborum inventore voluptas facere consequatur quibusdam, eos autem eum non sapiente? Hic vel voluptates sequi eum perferendis nostrum autem voluptas aspernatur minima, et voluptatibus, recusandae tempora asperiores laboriosam nulla molestias, provident accusantium! Alias debitis molestiae maxime enim, nemo aliquam, perferendis doloremque, eligendi quisquam magni quis? Qui, magni? Exercitationem error nisi corporis ipsa laboriosam dolorum voluptatibus, molestiae neque expedita odio. Sed autem repellendus ipsum dolorem magnam blanditiis ex consectetur porro quod, quasi temporibus illum placeat corporis natus ut expedita labore minus nemo. Eum provident velit nobis hic perspiciatis quas sapiente consequatur illum similique minus, cum nihil assumenda doloribus iste maxime eos dolores vel quod! Fuga et consectetur veniam assumenda illo optio, iusto iste maxime ea delectus, perspiciatis similique. Temporibus consequuntur quasi, corporis beatae fugiat, illo id neque ullam voluptate consequatur nemo dolores voluptas. Adipisci eum, error sit dolores quae ullam facere, laboriosam dolore aut, dolorem dignissimos deserunt magnam ipsam qui ex magni repudiandae ea temporibus culpa nobis. Recusandae corrupti dolores minus minima maxime natus illum, possimus earum itaque temporibus. Molestias, maxime quasi dignissimos vel debitis iusto illum facere labore corrupti sed quos est laudantium deleniti cum, voluptatem reprehenderit atque? Aliquam quis ut facilis nostrum provident porro illo reprehenderit, praesentium, officiis odio totam? Obcaecati expedita doloremque sapiente minima odit fugiat atque dolor, corrupti impedit aliquid iusto?
+          <div className=" collapse border border-warning" id="collapseExample1">
+            <div className=" info_content card card-body py-0">
+              <h5 className='text-center'>About ToDo and This website :-</h5>
+
+              <ol >
+                <li>
+                  ToDo app is used to maintain Your day-to-day tasks or list everything that you have to do today or tomorrow (<strong>Something that someone will need to do</strong>).
+                </li>
+
+                <li>
+                  In this web app, users can write <strong>tasks</strong> that he/her want to do today or tomorrow.
+                </li>
+
+                <li>
+                  Here users can write <strong>tasks and update it and delete it</strong>.
+                </li>
+
+                <li>
+                  One <strong> All delete button</strong> is present (In bottom center), so the user can delete his/her all unlock tasks in one go.
+                </li>
+
+                <li>
+                  After writing a task, the user can <strong> lock</strong> the task so that it will not be deleted with all delete button.
+                </li>
+
+                <li>
+                  After 2 hours , the user can see <strong> the time</strong> when he/she wrote the task (In the left side of tasks).
+                </li>
+
+                <li>
+                 <strong> Icons are</strong> :- <i className="fa-solid fa-square-plus" ></i> : Add , <i className="fa-solid fa-file-pen"></i> : Update , <i className="fa-solid fa-lock"></i> : Lock ,  <i className="fa-solid fa-delete-left"></i> : Delete ,  <i className="fa-sharp fa-solid fa-trash"></i> : All Delete.
+                </li>
+
+                <li>
+                  If you like this web app then you can post your feedback , by clicking on the <strong>feedback section button</strong>.
+                </li>
+
+
+                <li>
+                  Your tasks are stored in your browser's localstorage , so don't worry about that (<strong>Privacy First</strong>).
+                </li>
+
+                <p className='text-center '> <i class="fa-brands fa-youtube"></i> -: <a  href="https://www.youtube.com/" rel="noreferrer" target={"_blank"}>https://www.youtube.com/</a></p>
+              </ol>
             </div>
           </div>
         </div>
